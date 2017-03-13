4c9894f07fa77f6ecee56e5bf5f45fa78a4dd1b7
mcoenca <m.coenca@gmail.com>
2017-03-13 17:11:41 +0100

    using node-sass

--- gruntfile.js
+++ gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
   grunt.loadTasks('tasks');
   // These plugins provide necessary tasks.
 
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-browser-sync');
